60c32d2fd1129497067f00dfff47b4ce2f0d0322
Sara <saracorsob@gmail.com>
2020-05-21 16:47:52 +0200

    Inizio scrittura js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
+// Elementi form
 var formGenera = document.getElementById('form-genera');
 var formAnnulla = document.getElementById('form-annulla');
+var formNome = document.getElementById('form-nome');
+var formKm = parseInt(document.getElementById('form-km'));
+
+
+// Elementi biglietto
+var biglNome = document.getElementById('bigl-nome');
+
+// Evento Genera
+formGenera.addEventListener('click',
+  function() {
+    // Calcolo Costo
+    var prezzo = formKm.value * 0.21;
+
+    // Calcolo costo scontato
+    
+
+    // Scrittura Biglietto
+    biglNome.innerHTML = formNome.value;
+  }
+);
