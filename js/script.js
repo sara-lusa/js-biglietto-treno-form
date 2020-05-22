fb71376a050c2e3f005256be307d78f2d22759e8
Sara <saracorsob@gmail.com>
2020-05-22 16:52:03 +0200

    aggiunta errore al js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@ var btnAnnulla = document.getElementById('btn-annulla');
 var formNome = document.getElementById('form-nome');
 var formKm = document.getElementById('form-km');
 var formEta = document.getElementById('form-eta');
+var errore = document.getElementById('errore');
+
+console.log(formNome.value);
+console.log(formKm.value);
 
 // Elementi biglietto
 var biglietto = document.getElementById('biglietto');
@@ -18,43 +22,58 @@ var biglOfferta = document.getElementById('bigl-offerta');
 // Evento Button Genera
 btnGenera.addEventListener('click',
   function() {
-    // Calcolo Costo
-    var prezzo = parseInt(formKm.value) * 0.21;
-    biglOfferta.innerHTML = 'Biglietto Standard';
 
-    // Calcolo Costo Scontato
-    if( formEta.value == 'minorenne' ) {
-      prezzo = prezzo - ( prezzo * 20 / 100 )
-      biglOfferta.innerHTML = 'Biglietto Scontato per Minorenni';
+    // Dati incorretti
+    if ( isNaN(formKm.value) || formNome.value.lenght == 0 ) {
+      errore.className = 'block';
+    }
+    // Dati corretti
+    else {
+      // Calcolo Costo
+      var prezzo = parseInt(formKm.value) * 0.21;
+      biglOfferta.innerHTML = 'Biglietto Standard';
+
+      // Calcolo Costo Minorenni
+      if( formEta.value == 'minorenne' ) {
+        prezzo = prezzo - ( prezzo * 20 / 100 )
+        biglOfferta.innerHTML = 'Biglietto Scontato per Minorenni';
+      }
+      // Calcolo Costo Over 65
+      else if( formEta.value == 'over' ) {
+        prezzo = prezzo - ( prezzo * 40 / 100 )
+        biglOfferta.innerHTML = 'Biglietto Scontato per Over 65';
+      }
+
+      // Scrittura Biglietto
+      biglNome.innerHTML = formNome.value;
+      biglCosto.innerHTML = prezzo.toFixed(2) + ' Euro';
+      biglCarrozza.innerHTML = Math.floor(Math.random() * 9) + 1;
+      biglCodiceCp.innerHTML = Math.floor(Math.random() * 99999) + 1;
+
+      // Biglietto in vista
+      biglietto.className = 'biglietto block';
+    }
     }
 
-    else if( formEta.value == 'over' ) {
-      prezzo = prezzo - ( prezzo * 40 / 100 )
-      biglOfferta.innerHTML = 'Biglietto Scontato per Over 65';
-    }
-    // 
-    // else {
-    //
-    // }
-
-    // Scrittura Biglietto
-    biglNome.innerHTML = formNome.value;
-    biglCosto.innerHTML = prezzo.toFixed(2) + ' Euro';
-    biglCarrozza.innerHTML = Math.floor(Math.random() * 9) + 1;
-    biglCodiceCp.innerHTML = Math.floor(Math.random() * 99999) + 1;
-
-    // Biglietto in vista
-    biglietto.className = 'biglietto block';
-  }
 );
 
 // Evento Button Annulla
 btnAnnulla.addEventListener('click',
   function() {
+    // Nascondi errore
+    errore.className = 'hidden';
+
     // Reset Form
     formNome.value = '';
     formKm.value = '';
     formEta.value = 'maggiorenne';
+
+    // Reset Biglietto
+    biglNome.innerHTML = '';
+    biglCosto.innerHTML = '';
+    biglCarrozza.innerHTML = '';
+    biglCodiceCp.innerHTML = '';
+    biglOfferta.innerHTML = '';
 
     // Biglietto nascosto
     biglietto.className = 'biglietto hidden';
